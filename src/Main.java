4df96ffb3f69abf20fad24a201c500549e4f5c23
leiyu <yuleics@outlook.com>
2019-03-16 13:35:13 +0800

    first demo use channel

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.lesliefish.test01channel.ChannelTester;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java nio!");
+        ChannelTester.test();
+    }
+}
---
+++ src/com/lesliefish/test01channel/ChannelTester.java
@@ -0,0 +1,36 @@
+package com.lesliefish.test01channel;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelTester {
+    public static void test() {
+        String currentFilePath = System.getProperty("user.dir") + "/res/test.txt";
+        System.out.println(currentFilePath);
+        RandomAccessFile file = null;
+        try {
+            file = new RandomAccessFile(currentFilePath, "r");
+            FileChannel fileChannel = file.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+            while (fileChannel.read(byteBuffer) > 0) {
+                byteBuffer.flip(); // 调用flip之后，读写指针指到缓存头部
+                while (byteBuffer.hasRemaining()) {
+                    System.out.print((char) byteBuffer.get() + " ");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (file != null) {
+                    file.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
